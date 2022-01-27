f5c684b6a65848845cfc09dc23f9de34a1ffbf13
sebastian-medinaOch <sebastianm.o1123@gmail.com>
2022-01-27 09:46:37 -0500

    ACTUALIZACIÓN

--- config-service/src/main/java/com/smo/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/smo/configservice/ConfigServiceApplication.java
@@ -2,9 +2,8 @@ package com.smo.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
+
 @SpringBootApplication
 public class ConfigServiceApplication {
 

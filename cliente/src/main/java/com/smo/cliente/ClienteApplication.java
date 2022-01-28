09b3d5796a69212ea2be6d44c397b5f317af5802
sebastian-medinaOch <sebastianm.o1123@gmail.com>
2022-01-28 15:11:22 -0500

    REGISTRY SERVICE

--- cliente/src/main/java/com/smo/cliente/ClienteApplication.java
+++ cliente/src/main/java/com/smo/cliente/ClienteApplication.java
@@ -2,8 +2,10 @@ package com.smo.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClienteApplication {
 
 	public static void main(String[] args) {
